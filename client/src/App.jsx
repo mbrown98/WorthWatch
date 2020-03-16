a88d9c77a5cffe849ce275c560c73cb48539ca71
Matthew Brown <matthewbrown0798@gmail.com>
2020-03-16 14:49:27 -0400

    webpack working, html showing up in browser

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>Hello</div>;
+  }
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const path = require("path");
+const router = require("./routes");
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// app.use("/api/", router);
+
+app.listen(port, () => console.log(`Crypto-App ${port}!`));
