cbe48a5044fbad8776de1133c333523862eb0727
Matthew Brown <matthewbrown0798@gmail.com>
2020-03-19 21:25:37 -0400

    can now search any stock by daily, weekly, monthly

--- client/src/components/Stocks.jsx
+++ client/src/components/Stocks.jsx
@@ -8,39 +8,80 @@ export default class Stocks extends React.Component {
     super(props);
     this.state = {
       stockSearch: "",
-      searchedData: []
+      searchedData: [],
+      time: "DAIlY",
+      dailyValues: null,
+      monthlyValues: null,
+      weeklyValues: null
     };
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+
     this.getHistStockInfo = this.getHistStockInfo.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ stockSearch: event.target.value });
+    this.setState({
+      stockSearch: event.target.value,
+      dailyValues: null,
+      monthlyValues: null,
+      weeklyValues: null
+    });
   }
 
-  handleSubmit(event) {
-    console.log(this.state.stockSearch);
-    this.getHistStockInfo(this.state.stockSearch);
-    event.preventDefault();
-  }
-
-  getHistStockInfo(name) {
+  getHistStockInfo() {
+    console.log("serarch", this.state.stockSearch);
     axios
       .get(
-        `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${name}&apikey=X56VBGT3RVIY8K94`
+        `https://www.alphavantage.co/query?function=TIME_SERIES_${this.state.time}&symbol=${this.state.stockSearch}&apikey=X56VBGT3RVIY8K94`
       )
       .then(response => {
-        let histData = response.data["Time Series (Daily)"];
-        let obj = {};
-        let initialDates = Object.keys(histData);
-        obj.dates = initialDates.reverse();
-        let pricesRawInitial = Object.values(histData);
-        let pricesRaw = pricesRawInitial.reverse();
-        obj.prices = pricesRaw.map(element => Number(element["2. high"]));
-        return obj;
+        let histData = "";
+        if (this.state.time === "DAILY") {
+          if (this.state.dailyValues !== null) {
+            return this.state.dailyValues;
+          }
+          histData = response.data["Time Series (Daily)"];
+          let obj = {};
+          let initialDates = Object.keys(histData);
+          obj.dates = initialDates.reverse();
+          let pricesRawInitial = Object.values(histData);
+          let pricesRaw = pricesRawInitial.reverse();
+          obj.name = this.state.stockSearch;
+          obj.prices = pricesRaw.map(element => Number(element["2. high"]));
+          this.setState({ dailyValues: obj });
+          return obj;
+        } else if (this.state.time === "WEEKLY") {
+          if (this.state.weeklyValues !== null) {
+            return this.state.weeklyValues;
+          }
+          histData = response.data["Weekly Time Series"];
+          let obj = {};
+          let initialDates = Object.keys(histData).slice(0, 20);
+          obj.dates = initialDates.reverse();
+          let pricesRawInitial = Object.values(histData).slice(0, 20);
+          let pricesRaw = pricesRawInitial.reverse();
+          obj.name = this.state.stockSearch;
+          obj.prices = pricesRaw.map(element => Number(element["2. high"]));
+          this.setState({ weeklyValues: obj });
+          return obj;
+        } else if (this.state.time === "MONTHLY") {
+          if (this.state.monthlyValues !== null) {
+            return this.state.monthlyValues;
+          }
+          histData = response.data["Monthly Time Series"];
+          let obj = {};
+          let initialDates = Object.keys(histData).slice(0, 20);
+          obj.dates = initialDates.reverse();
+          let pricesRawInitial = Object.values(histData).slice(0, 20);
+          let pricesRaw = pricesRawInitial.reverse();
+          obj.name = this.state.stockSearch;
+          obj.prices = pricesRaw.map(element => Number(element["2. high"]));
+          this.setState({ monthlyValues: obj });
+          return obj;
+        }
       })
       .then(obj => {
+        console.log(obj);
         const node = this.node;
         var newChart = new Chart(node, {
           type: "line",
@@ -48,7 +89,7 @@ export default class Stocks extends React.Component {
             labels: obj.dates,
             datasets: [
               {
-                label: "Stock",
+                label: obj.name,
                 data: obj.prices,
                 backgroundColor: [
                   "rgba(5, 120, 133333, 0.4)",
@@ -57,6 +98,9 @@ export default class Stocks extends React.Component {
                 ]
               }
             ]
+          },
+          options: {
+            maintainAspectRatio: false
           }
         });
       });
@@ -73,6 +117,10 @@ export default class Stocks extends React.Component {
               <div class="hero-body">
                 <div class="container">
                   <h1 class="title">StockWatch</h1>
+                  <h4 class="subtitle">
+                    Based on API limits you can search full data for one stock
+                    per minute
+                  </h4>
                 </div>
               </div>
             </section>
@@ -88,46 +136,47 @@ export default class Stocks extends React.Component {
                   onChange={this.handleChange}
                 />
               </label>
-              <input type="submit" value="Submit" />
             </form>
           </div>
         </div>
-        <div>
-          <table class="table has-text-left">
-            <thead>
-              <th></th>
-
-              <th>Symbol</th>
-              <th>Price</th>
-              <th>Day High</th>
-              <th>Day Low</th>
-              <th>Day Change</th>
-              <th>52 Week High</th>
-              <th>52 Week Low</th>
-            </thead>
-            <tbody>
-              {this.state.searchedData.map(stock => {
-                return (
-                  <tr>
-                    <th>
-                      <a onClick={() => console.log("hello")}>{stock.name}</a>
-                    </th>
-                    <td>{stock.symbol}</td>
-                    <td>{stock.price}</td>
-                    <td>{stock.day_high}</td>
-                    <td>{stock.day_low}</td>
-                    <td>{stock.day_change}</td>
-                    <td>{stock.hist_high}</td>
-                    <td>{stock.hist_low}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+        <div className="field is-grouped">
+          {/* <a
+            className="button is-danger"
+            onClick={() => {
+              console.log("hello");
+            }}
+          >
+            Hourly
+          </a> */}
+          <a
+            className="button is-link"
+            onClick={() => {
+              this.setState({ time: "DAILY" }, () => this.getHistStockInfo());
+            }}
+          >
+            Daily
+          </a>
+          <a
+            className="button is-primary"
+            onClick={() => {
+              this.setState({ time: "WEEKLY" }, () => this.getHistStockInfo());
+            }}
+          >
+            Weekly
+          </a>
+          <a
+            className="button is-primary"
+            onClick={() => {
+              console.log("hellllloooo");
+              this.setState({ time: "MONTHLY" }, () => this.getHistStockInfo());
+            }}
+          >
+            Monthly
+          </a>
         </div>
         <div>
           {" "}
-          <canvas id="theChart" ref={node => (this.node = node)} />
+          <canvas id="stockChart" ref={node => (this.node = node)} />
         </div>
       </div>
     );
@@ -147,3 +196,14 @@ export default class Stocks extends React.Component {
 // console.log(obj);
 // var newSearchedData = [...this.state.searchedData, obj];
 // this.setState({ searchedData: newSearchedData });
+
+// hourly: https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo
+
+// daily: https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo
+// weekly: https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=MSFT&apikey=demo
+// monthly: https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=MSFT&apikey=demo
+
+// hourly: INTRADAY & interval=5min
+// daily: DAILY
+// weekly: WEEKLY
+// monthly: MONTHLY
--- client/src/components/Watching.jsx
+++ client/src/components/Watching.jsx
@@ -30,6 +30,39 @@ export default class Watching extends React.Component {
             </div>
           </section>
         </div>
+        <div>
+          <table class="table has-text-left">
+            <thead>
+              <th></th>
+
+              <th>Symbol</th>
+              <th>Price</th>
+              <th>Day High</th>
+              <th>Day Low</th>
+              <th>Day Change</th>
+              <th>52 Week High</th>
+              <th>52 Week Low</th>
+            </thead>
+            <tbody>
+              {/* {this.state.searchedData.map(stock => {
+                return (
+                  <tr>
+                    <th>
+                      <a onClick={() => console.log("hello")}>{stock.name}</a>
+                    </th>
+                    <td>{stock.symbol}</td>
+                    <td>{stock.price}</td>
+                    <td>{stock.day_high}</td>
+                    <td>{stock.day_low}</td>
+                    <td>{stock.day_change}</td>
+                    <td>{stock.hist_high}</td>
+                    <td>{stock.hist_low}</td>
+                  </tr>
+                );
+              })} */}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
